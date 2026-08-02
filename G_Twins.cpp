6005927f6932702218fc09e85c0514c062632b50
maruf0237 <marufulislam0098@gmail.com>
2026-08-02 11:02:19 +0600

    Twins.cpp upadated

--- G_Twins.cpp
+++ G_Twins.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<int> coins(n);
+    int total = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> coins[i];
+        total += coins[i];
+    }
+
+    sort(coins.begin(), coins.end(), greater<int>());
+
+    int mySum = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        mySum += coins[i];
+        cnt++;
+
+        if (mySum > total - mySum)
+        {
+            cout << cnt;
+            break;
+        }
+    }
 
     return 0;
 }
